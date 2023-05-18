c09125c8b3d370fda9f9e819d0c19ad72f0def8c
rina shkrabova <eshkrab@gmail.com>
2023-05-18 12:51:04 -0700

    async videos handler

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
 import os
 import json
+import zmq
+import zmq.asyncio
 from queue import Queue
 from quart import Quart, websocket, send_file, request, jsonify
 from quart_cors import cors, route_cors
@@ -17,70 +19,87 @@ ws_queue = Queue(maxsize=10)
 
 ALLOWED_EXTENSIONS = {"mp4", "avi", "mov"}
 
+ctx = zmq.asyncio.Context()
+socket = ctx.socket(zmq.REQ)  # REQ type socket for ZMQ
+socket.connect("tcp://localhost:5555")  # Connect to the player app
+
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
+async def send_message_to_player(message):
+    await socket.send_string(message)
+    reply = await socket.recv_string()  # Waiting for reply
+    return reply
+
 # Content player REST API
 @app.route("/api/state", methods=["GET", "POST"])
-def set_state():
+async def set_state():
     if request.method == "POST":
         state = request.form.get("state")
-        print(f"Received state: {state}")
-        if state == "start":
-            print('play')
-        elif state == "stop":
-            print('stop stop ')
-        elif state == "pause":
-            print('pause')
-        elif state == "next":
-            print('next')
-        elif state == "prev":
-            print('prev')
-        elif state == "restart":
-            print('restart')
-        else:
-            return jsonify({"error": "Invalid state"}), 400
-    elif request.method == "POST":
-        pass
-    return jsonify({"success": True})
+        reply = await send_message_to_player(state)
+        print(f"Reply from player: {reply}")
+        return jsonify({"success": True, "reply": reply})
 
 @app.route("/api/playlist", methods=["GET", "POST"])
-def handle_playlist():
+async def handle_playlist():
     if request.method == "GET":
-        pass
-        #  with open(video_player.playlist_path, "r") as f:
-        #      playlist = json.load(f)
-        #  return jsonify(playlist)
-    elif request.method == "POST":
-        #  playlist = request.json
-        #  with open(video_player.playlist_path, "w") as f:
-        #      json.dump(playlist, f)
-        #  #  video_player.populate_video_queue()
-        #  video_player.stop()
-        #  video_player.play()
-        return jsonify({"success": True})
+        reply = await send_message_to_player("get_playlist")
+        return jsonify({"playlist": reply})
+
 
 @app.route("/api/brightness", methods=["GET", "POST"])
 @route_cors(allow_origin="*")
 async def handle_brightness():
-    if request.method == "GET":
-        return jsonify({"brightness": 255})
-    elif request.method == "POST":
+    if request.method == "POST":
         brightness = request.form.get("brightness")
-        if brightness is None:
-            return jsonify({"error": "Brightness value is missing"}), 400
-        try:
-            brightness = int(brightness)
-        except ValueError:
-            return jsonify({"error": "Invalid brightness value"}), 400
-        #  video_player.display.brightness_level = brightness
-        return jsonify({"success": True})
+        reply = await send_message_to_player(f"set_brightness {brightness}")
+        return jsonify({"success": True, "reply": reply})
+
 
 @app.route('/api/fps', methods=['GET', 'POST'])
 @route_cors(allow_origin="*")
 async def set_fps():
     # Insert logic to adjust FPS
     return 'FPS adjusted'
+
+@app.route("/api/videos", methods=['GET', 'POST', 'DELETE'])
+@route_cors(allow_origin="*")
+async def handle_videos():
+    if request.method == "GET":
+        videos = [
+            f
+            for f in os.listdir(video_dir)
+            if os.path.isfile(os.path.join(video_dir, f)) and allowed_file(f)
+        ]
+        return jsonify({"videos": videos})
+
+    elif request.method == "POST":
+        if "file" not in (await request.files):
+            return jsonify({"error": "No file part"}), 400
+        file = (await request.files)["file"]
+        if file.filename == "":
+            return jsonify({"error": "No file selected"}), 400
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            await file.save(os.path.join(video_dir, filename))
+            return jsonify({"success": True})
+        else:
+            return jsonify({"error": "Unsupported file type"}), 400
+
+    elif request.method == "DELETE":
+        filename = (await request.form).get("filename")
+        if filename is None:
+            return jsonify({"error": "Filename is missing"}), 400
+        file_path = os.path.join(video_dir, filename)
+        if os.path.exists(file_path):
+            os.remove(file_path)
+            return jsonify({"success": True})
+        else:
+            return jsonify({"error": "File not found"}), 404
+
+    else:
+        return jsonify({"error": "Unsupported method"}), 405
+
 
 
 @app.route("/api/videos", methods=['GET', 'POST', 'DELETE'])
